99f07a76914517597aed0710992261acaf20ffcd
Mostafa Mohamed Mousa <mostafamousa1080p@gmail.com>
2018-02-22 07:25:38 +0200

    Some New Staff

--- FileWorking.py
+++ FileWorking.py
@@ -4,26 +4,26 @@ from BubleSort import bubble_sort
 import time
 import random
 RequaredRanges = [10000]
-#RequaredRanges = [10, 100, 500, 1000, 100000, 500000, 1000000, 10000000]
+#RequaredRangesSmall = [10, 100, 500, 1000,10000,100000, 500000, 1000000, 10000000]
 index = 0
 def WriteNumbersinFile(n):
     global index
     Randomied = []
     #Generating Random Numbers
     Number = n[index]
-    Write = open("Numbers.txt",'a')
+    Write = open("Small_Numbers.txt",'a')
     #Write.write(str(Number)+" ")
     for x in range(0,Number):
         if x<Number-1:
-         Write.write((str(random.randint(1,1000))+" "))
+         Write.write((str(random.randint(1,10000))+" "))
         else:
-            Write.write((str(random.randint(1, 1000))))
+            Write.write((str(random.randint(1, 10000))))
     if index==8:
      index=0
     else:index+=1
 
 def HatMenFile():
-    x = open("Numbers.txt", 'r')
+    x = open("Small_Numbers.txt", 'r')
 
     for Number in x:
         Number
@@ -35,27 +35,32 @@ def HatMenFile():
 
 
 WriteNumbersinFile(RequaredRanges)
-
+fes = HatMenFile()
+es  = HatMenFile()
+bs= HatMenFile()
 for x in range(0, 1):
-        print("QuickSort")
-        print(RequaredRanges[x])
+        WriteResult = open("Result.txt" ,"w")
+
+        WriteResult.write("QuickSort\n")
+        WriteResult.write(str(RequaredRanges[x])+'\n')
         t3 = time.time()
 
-        QuickSort(HatMenFile())
+        QuickSort(fes)
         t4 = time.time() - t3
-        print(t4)
+        WriteResult.write(str(t4)+'\n')
 
-        print("Insertions Sort")
-        print(RequaredRanges[x])
-        t1 = time.time()
-        print(Insertion_sort(HatMenFile()))
-        t2 = time.time() - t1
-        print(t2)
-        print("Buble Sort")
-        print(RequaredRanges[x])
-        t1 = time.time()
-        print(Insertion_sort(HatMenFile()))
-        t2 = time.time() - t1
-        print(t2)
+        WriteResult.write("Buble Sort"+'\n')
+        WriteResult.write(str(RequaredRanges[x])+'\n')
+        t5 = time.time()
+        bubble_sort(bs)
+        t6 = time.time() - t5
+        WriteResult.write(str(t6)+'\n')
 
+
+        WriteResult.write("Insertions Sort\n")
+        WriteResult.write(str(RequaredRanges[x])+'\n')
+        t1 = time.time()
+        Insertion_sort(es)
+        t2 = time.time() - t1
+        WriteResult.write(str(t2)+'\n')
 
